6a9ef4ba67801ce1667039f067c8274be9e706a8
Alexander Van hoof <69984399+vanhoofa3995@users.noreply.github.com>
2021-02-23 17:27:09 -0500

    Update Main.java
    Main Code Update

--- Prototypes/ApplicationStartPage/Main.java
+++ Prototypes/ApplicationStartPage/Main.java
@@ -4,14 +4,12 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        //LoginForm frame = new LoginForm();
-        //frame.setTitle("Login Form");
-        //frame.setVisible(true);
-        //frame.setBounds(10, 10, 370, 600);
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setResizable(false);
-        //creating object of Registration class
-        new RegistrationForm();
+        LoginForm frame = new LoginForm();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 370, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
     }
     
 }
