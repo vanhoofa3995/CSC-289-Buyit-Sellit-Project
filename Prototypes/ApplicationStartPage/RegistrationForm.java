6a397b592fbff1fdef9d829814793172a0a9fbca
Alexander Van hoof <69984399+vanhoofa3995@users.noreply.github.com>
2021-03-03 16:27:04 -0500

    Update RegistrationForm.java
    Updated version of the registration form.

--- Prototypes/ApplicationStartPage/RegistrationForm.java
+++ Prototypes/ApplicationStartPage/RegistrationForm.java
@@ -12,6 +12,7 @@ public class RegistrationForm implements ActionListener {
     JLabel lastNameLabel=new JLabel("LAST NAME");
     JLabel firstNameLabel=new JLabel("FIRST NAME");
     JLabel emailLabel=new JLabel("EMAIL");
+    JLabel userNameLabel=new JLabel("USER NAME");
     JLabel passwordLabel=new JLabel("PASSWORD");
     JLabel confirmPasswordLabel=new JLabel("CONFIRM PASSWORD");
     JLabel securityQuestionLabel=new JLabel("SECUIRITY QUESTION");
@@ -20,12 +21,16 @@ public class RegistrationForm implements ActionListener {
     JTextField lastNameTextField=new JTextField();
     JTextField firstNameTextField=new JTextField();
     JTextField emailTextField=new JTextField();
+    JTextField userNameField=new JTextField();
     JPasswordField passwordField=new JPasswordField();
     JPasswordField confirmPasswordField=new JPasswordField();
     JTextField securityQuestionTextField=new JTextField();
     JTextField securityAnswerTextField=new JTextField();
     JButton registerButton=new JButton("REGISTER");
     JButton resetButton=new JButton("RESET");
+    JButton loginButton = new JButton("LOGIN");
+    JButton forgotPasswordButton = new JButton("FORGOT PASSWORD");
+    
     
     
     //Creating constructor
@@ -54,26 +59,31 @@ public class RegistrationForm implements ActionListener {
         lastNameLabel.setBounds(20,20,100,70);
         firstNameLabel.setBounds(20,70,100,70);
         emailLabel.setBounds(20,120,100,70);
-        passwordLabel.setBounds(20,170,100,70);
-        confirmPasswordLabel.setBounds(20,220,140,70);
-        securityQuestionLabel.setBounds(20,270,150,70);
-        securityAnswerLabel.setBounds(20,320,150,70);
+        userNameLabel.setBounds(20,170,100,70);
+        passwordLabel.setBounds(20,220,100,70);
+        confirmPasswordLabel.setBounds(20,270,140,70);
+        securityQuestionLabel.setBounds(20,320,150,70);
+        securityAnswerLabel.setBounds(20,370,150,70);
         
         lastNameTextField.setBounds(180,43,185,23);
         firstNameTextField.setBounds(180,93,185,23);
         emailTextField.setBounds(180,143,185,23);
-        passwordField.setBounds(180,193,185,23);
-        confirmPasswordField.setBounds(180,243,185,23);
-        securityQuestionTextField.setBounds(180,293,185,23);
-        securityAnswerTextField.setBounds(180,343,185,23);
-        registerButton.setBounds(70,400,100,35);
-        resetButton.setBounds(220,400,100,35);
+        userNameField.setBounds(180,193,185,23);
+        passwordField.setBounds(180,243,185,23);
+        confirmPasswordField.setBounds(180,293,185,23);
+        securityQuestionTextField.setBounds(180,343,185,23);
+        securityAnswerTextField.setBounds(180,393,185,23);
+        registerButton.setBounds(15,457,160,30);
+        resetButton.setBounds(185,457,160,30);
+        loginButton.setBounds(15,497,160,30);
+        forgotPasswordButton.setBounds(185,497,160,30);
     }
     public void addComponentsToFrame()
     {
         frame.add(lastNameLabel);
         frame.add(firstNameLabel);
         frame.add(emailLabel);
+        frame.add(userNameLabel);
         frame.add(passwordLabel);
         frame.add(confirmPasswordLabel);
         frame.add(securityQuestionLabel);
@@ -82,17 +92,23 @@ public class RegistrationForm implements ActionListener {
         frame.add(lastNameTextField);
         frame.add(firstNameTextField);
         frame.add(emailTextField);
+        frame.add(userNameField);
         frame.add(passwordField);
         frame.add(confirmPasswordField);
         frame.add(securityQuestionTextField);
         frame.add(securityAnswerTextField);
         frame.add(registerButton);
         frame.add(resetButton);
+        frame.add(loginButton);
+        frame.add(forgotPasswordButton);
     }
     public void actionEvent()
     {
         registerButton.addActionListener(this);
         resetButton.addActionListener(this);
+        loginButton.addActionListener(this);
+        forgotPasswordButton.addActionListener(this);
+                
     }
     
 @Override
@@ -100,18 +116,20 @@ public class RegistrationForm implements ActionListener {
         if(e.getSource()==registerButton)
         {
             try {
-                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/GamingDomainDataBase","VanHoofAlex","password");
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
                 PreparedStatement Pstatement=connection.prepareStatement("INSERT INTO" 
-                        + " userProfile(lastName,firstName,email,password," 
-                        + "confirmPassword,securityQuestion,securityAnswer)" 
+                        + " userProfile(lastName,firstName,email,userName," 
+                        + "password,securityQuestion,securityAnswer)" 
                         + " VALUES (?, ?, ?, ?, ?, ?, ?)");
                 Pstatement.setString(1,lastNameTextField.getText());
                 Pstatement.setString(2,firstNameTextField.getText());
                 Pstatement.setString(3,emailTextField.getText());
-                Pstatement.setString(4,passwordField.getText());
-                Pstatement.setString(5,confirmPasswordField.getText());
+                Pstatement.setString(4,userNameField.getText());
+                Pstatement.setString(5,passwordField.getText());
                 Pstatement.setString(6,securityQuestionTextField.getText());
                 Pstatement.setString(7,securityAnswerTextField.getText());
+                
+                
                 if(passwordField.getText().equalsIgnoreCase(confirmPasswordField.getText()))
                 {
 
@@ -134,10 +152,26 @@ public class RegistrationForm implements ActionListener {
             lastNameTextField.setText("");
             firstNameTextField.setText("");
             emailTextField.setText("");
+            userNameField.setText("");
             passwordField.setText("");
             confirmPasswordField.setText("");
             securityQuestionTextField.setText("");
             securityAnswerTextField.setText("");
+        }
+        
+        if (e.getSource() == loginButton) {
+            frame.dispose();    
+            LoginForm frame = new LoginForm();
+            frame.setTitle("Login Form");
+            frame.setVisible(true);
+            frame.setBounds(10, 10, 370, 600);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);  
+        }
+        
+        if (e.getSource() == forgotPasswordButton) {
+           frame.dispose();
+           new ForgotPasswordForm();
         }
 
     }
