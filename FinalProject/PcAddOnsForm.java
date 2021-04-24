817a87f9623a4bddb068864240ec4ca18f5f67de
Alexander Van hoof <69984399+vanhoofa3995@users.noreply.github.com>
2021-04-24 09:26:09 -0400

    Create PcAddOnsForm.java

---
+++ FinalProject/PcAddOnsForm.java
@@ -0,0 +1,304 @@
+/*
+ * @author vanhoofa3995
+ * @CSC-289-0B01
+ * @Last Edit 4/24/21
+ */
+
+package applicationstartpage;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.sql.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PcAddOnsForm implements ActionListener {
+    JFrame frame;
+    
+    //Creating objects
+    JLabel pcCategoryLabel = new JLabel("PC ADD-ONS");
+    
+    JButton pcItemButton1 = new JButton("STICKERS");
+    JButton addToCartButton1 = new JButton("ADD");
+    JButton removeFromCartButton1 = new JButton("REM");
+    
+    JButton pcItemButton2 = new JButton("LIGHTS");
+    JButton addToCartButton2 = new JButton("ADD");
+    JButton removeFromCartButton2 = new JButton("REM");
+    
+    JButton pcItemButton3 = new JButton("POP FIGURES");
+    JButton addToCartButton3 = new JButton("ADD");
+    JButton removeFromCartButton3 = new JButton("REM");
+    
+    JButton pcItemButton4 = new JButton("DECORATIONS");
+    JButton addToCartButton4 = new JButton("ADD");
+    JButton removeFromCartButton4 = new JButton("REM");
+    
+    JButton categoryReturnButton = new JButton("RETURN");
+        
+    //Creating constructor
+    PcAddOnsForm()
+    {
+        createWindow();
+        setLocationAndSize();
+        addComponentsToFrame();
+        actionEvent();
+    }
+    
+    public void createWindow()
+    {
+        frame=new JFrame();
+        frame.setTitle("Pc Add-Ons Form");
+        frame.setBounds(40,40,400,600);
+        frame.getContentPane().setBackground(Color.orange);
+        frame.getContentPane().setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+    }
+    
+    public void setLocationAndSize()
+    {
+        pcCategoryLabel.setBounds(150, 10, 140, 30);
+        
+        pcItemButton1.setBounds(20, 80, 140, 30);
+        addToCartButton1.setBounds(20, 110, 70, 20);
+        removeFromCartButton1.setBounds(90, 110, 70, 20);
+        
+        pcItemButton2.setBounds(20, 150, 140, 30);
+        addToCartButton2.setBounds(20, 180, 70, 20);
+        removeFromCartButton2.setBounds(90, 180, 70, 20);
+        
+        pcItemButton3.setBounds(220, 80, 140, 30); 
+        addToCartButton3.setBounds(220, 110, 70, 20);
+        removeFromCartButton3.setBounds(290, 110, 70, 20);
+        
+        pcItemButton4.setBounds(220, 150, 140, 30); 
+        addToCartButton4.setBounds(220, 180, 70, 20);
+        removeFromCartButton4.setBounds(290, 180, 70, 20);
+        
+        categoryReturnButton.setBounds(120, 220, 140, 30);               
+        
+    }
+    public void addComponentsToFrame()
+    {
+        frame.add(pcCategoryLabel);
+        
+        frame.add(pcItemButton1);
+        frame.add(addToCartButton1);
+        frame.add(removeFromCartButton1);
+        
+        frame.add(pcItemButton2);
+        frame.add(addToCartButton2);
+        frame.add(removeFromCartButton2);
+        
+        frame.add(pcItemButton3);
+        frame.add(addToCartButton3);
+        frame.add(removeFromCartButton3);
+        
+        frame.add(pcItemButton4);
+        frame.add(addToCartButton4);
+        frame.add(removeFromCartButton4);
+        
+        frame.add(categoryReturnButton);
+        
+    }
+    public void actionEvent()
+    {
+        pcItemButton1.addActionListener(this);
+        addToCartButton1.addActionListener(this);
+        removeFromCartButton1.addActionListener(this);
+        
+        pcItemButton2.addActionListener(this);
+        addToCartButton2.addActionListener(this);
+        removeFromCartButton2.addActionListener(this);
+        
+        pcItemButton3.addActionListener(this);
+        addToCartButton3.addActionListener(this);
+        removeFromCartButton3.addActionListener(this);
+        
+        pcItemButton4.addActionListener(this);
+        addToCartButton4.addActionListener(this);
+        removeFromCartButton4.addActionListener(this);
+        
+        categoryReturnButton.addActionListener(this);
+        
+        
+    }
+    
+@Override
+
+public void actionPerformed(ActionEvent e) {
+        //Coding Part of RETURN button
+        if (e.getSource() == categoryReturnButton) {
+           frame.dispose();
+           new BrowsingStockForm();
+        }
+        
+        //Coding Part of pcItemButton1
+        if (e.getSource() == pcItemButton1) {
+           JOptionPane.showMessageDialog(null,"Stickers\n"
+                   + "Includes 10 random game series stickers.\n"
+                   + "Can be attached by removing the adhesive layer piece.\n"
+                   + "Being sold for $10.00.");
+        }
+        
+        if(e.getSource()==addToCartButton1)
+        {
+            try {
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
+                PreparedStatement Pstatement=connection.prepareStatement("INSERT INTO" 
+                        + " userCart"
+                        + " VALUES ('Stickers', 'Pc', 1, 10.00)");
+                Pstatement.executeUpdate();
+                JOptionPane.showMessageDialog(null,"Item Added To Cart!");
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+        
+        if(e.getSource()==removeFromCartButton1)
+        {
+            try {
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
+                PreparedStatement Pstatement=connection.prepareStatement("DELETE FROM" 
+                        + " userCart"
+                        + " WHERE itemName = 'Stickers' AND itemPlatform = 'Pc'");
+                Pstatement.executeUpdate();
+                JOptionPane.showMessageDialog(null,"Item Removed From Cart!");
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+            
+        //Coding Part of pcItemButton2
+        if (e.getSource() == pcItemButton2) {
+           JOptionPane.showMessageDialog(null,"Lights\n"
+                   + "Includes 2 5ft multi color light strips.\n"
+                   + "Can be attached by removing the adhesive layer piece.\n"
+                   + "Being sold for $25.00.");
+        }
+        
+        if(e.getSource()==addToCartButton2)
+        {
+            try {
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
+                PreparedStatement Pstatement=connection.prepareStatement("INSERT INTO" 
+                        + " userCart"
+                        + " VALUES ('Lights', 'Pc', 1, 25.00)");
+                Pstatement.executeUpdate();
+                JOptionPane.showMessageDialog(null,"Item Added To Cart!");
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+        
+        if(e.getSource()==removeFromCartButton2)
+        {
+            try {
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
+                PreparedStatement Pstatement=connection.prepareStatement("DELETE FROM" 
+                        + " userCart"
+                        + " WHERE itemName = 'Lights' AND itemPlatform = 'Pc'");
+                Pstatement.executeUpdate();
+                JOptionPane.showMessageDialog(null,"Item Removed From Cart!");
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+        
+        //Coding Part of pcItemButton3
+        if (e.getSource() == pcItemButton3) {
+           JOptionPane.showMessageDialog(null,"Pop Figures\n"
+                   + "Includes 1 random game pop character.\n"
+                   + "Great for a gamer type decoration.\n"
+                   + "Being sold for $15.00.");
+        }
+        
+        if(e.getSource()==addToCartButton3)
+        {
+            try {
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
+                PreparedStatement Pstatement=connection.prepareStatement("INSERT INTO" 
+                        + " userCart"
+                        + " VALUES ('Pop Figures', 'Pc', 1, 15.00)");
+                Pstatement.executeUpdate();
+                JOptionPane.showMessageDialog(null,"Item Added To Cart!");
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+        
+        if(e.getSource()==removeFromCartButton3)
+        {
+            try {
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
+                PreparedStatement Pstatement=connection.prepareStatement("DELETE FROM" 
+                        + " userCart"
+                        + " WHERE itemName = 'Pop Figures' AND itemPlatform = 'Pc'");
+                Pstatement.executeUpdate();
+                JOptionPane.showMessageDialog(null,"Item Removed From Cart!");
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+        
+        //Coding Part of pcItemButton4
+        if (e.getSource() == pcItemButton4) {
+           JOptionPane.showMessageDialog(null,"Decorations\n"
+                   + "Includes a variety of lights, figures, stickers, and gamer type editions.\n"
+                   + "Being sold for $50.00.");
+        }
+        
+        if(e.getSource()==addToCartButton4)
+        {
+            try {
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
+                PreparedStatement Pstatement=connection.prepareStatement("INSERT INTO" 
+                        + " userCart"
+                        + " VALUES ('Decorations', 'Pc', 1, 50.00)");
+                Pstatement.executeUpdate();
+                JOptionPane.showMessageDialog(null,"Item Added To Cart!");
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+        
+        if(e.getSource()==removeFromCartButton4)
+        {
+            try {
+                Connection connection=DriverManager.getConnection("jdbc:derby://localhost:1527/MyGamingDomainDataBase","VanHoofAlex","password");
+                PreparedStatement Pstatement=connection.prepareStatement("DELETE FROM" 
+                        + " userCart"
+                        + " WHERE itemName = 'Decorations' AND itemPlatform = 'Pc'");
+                Pstatement.executeUpdate();
+                JOptionPane.showMessageDialog(null,"Item Removed From Cart!");
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+    }
+}
